9c9133ff9eff761b3321837dabd6128558788189
Vladimir Kupershtein <vladkupershtein@gmail.com>
2025-09-21 10:23:49 +0200

    Add central crop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tempfile
 import io
 import uuid
+import cv2
 import numpy as np
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.responses import Response
@@ -15,7 +16,7 @@ from anomalib.models import Patchcore
 # === Initialize FastAPI ===
 app = FastAPI(title="Anomaly Detection API")
 
-# === Загружаем модель один раз при старте ===
+# === Load model ===
 ckpt_path = "model/model.ckpt"
 
 pre_processor = Patchcore.configure_pre_processor(image_size=(256, 256))
@@ -40,18 +41,39 @@ class PredictResponse(BaseModel):
     score: float
     label: int
 
+def center_crop_resize_bytes(image_bytes: bytes, size: int = 256) -> np.ndarray:
+    # read from bytes
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    if image is None:
+        raise ValueError("Does not work")
+
+    h, w = image.shape[:2]
+    min_dim = min(h, w)
+
+    # centere coordinates
+    start_x = (w - min_dim) // 2
+    start_y = (h - min_dim) // 2
+    cropped = image[start_y:start_y + min_dim, start_x:start_x + min_dim]
+
+    # resize в (size,size)
+    resized = cv2.resize(cropped, (size, size))
+    return resized
+
 
 @app.post("/predict", response_model=PredictResponse)
 async def predict(
     file: UploadFile = File(...),
-    threshold: float = Form(0.5)
+    threshold: float = Form(0.3)
 ):
     """Upload image, run the model, return score/label/id."""
 
     contents = await file.read()
+    img = center_crop_resize_bytes(contents)
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp:
-        tmp.write(contents)
         tmp_path = tmp.name
+        cv2.imwrite(tmp_path, img)      
 
     # inference
     result = engine.predict(model=model, ckpt_path=ckpt_path, data_path=tmp_path)
@@ -76,16 +98,16 @@ async def predict(
 
 @app.get("/anomaly_map/{uid}")
 async def get_anomaly_map(uid: str):
-    """Return anomaly map by uid as PNG (без OpenCV)."""
+    """Return anomaly map by uid as PNG (without OpenCV)."""
     if uid not in results_store:
         raise HTTPException(status_code=404, detail="ID not found")
 
     anomaly_map = results_store[uid]["anomaly_map"]
 
-    # Убираем лишние оси
+    # Remove axis
     anomaly_map = np.squeeze(anomaly_map)
 
-    # Нормализация [0,1] -> [0,255]
+    # Normalization [0,1] -> [0,255]
     anomaly_map_norm = (255 * (anomaly_map - anomaly_map.min()) /
                         (anomaly_map.max() - anomaly_map.min() + 1e-8))
     anomaly_map_uint8 = anomaly_map_norm.astype(np.uint8)
